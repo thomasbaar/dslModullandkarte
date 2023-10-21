e3f67ca23a322ae34850b57a96fff3c77bf29f2d
Thomas Baar <thomas.baar@htw-berlin.de>
2023-10-21 16:24:08 +0200

    domain model slightly changed, implemented as grammar,example folder provided

--- mlk/src/language/modul-land-karte-validator.ts
+++ mlk/src/language/modul-land-karte-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { ModulLandKarteAstType, Person } from './generated/ast.js';
+import type { ModulLandKarteAstType, Model } from './generated/ast.js';
 import type { ModulLandKarteServices } from './modul-land-karte-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: ModulLandKarteServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.ModulLandKarteValidator;
     const checks: ValidationChecks<ModulLandKarteAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        Model: validator.checkModelDummy
     };
     registry.register(checks, validator);
 }
@@ -19,12 +19,9 @@ export function registerValidationChecks(services: ModulLandKarteServices) {
  */
 export class ModulLandKarteValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
+    checkModelDummy(model: Model, accept: ValidationAcceptor): void {
+        if (model.name) {
+            accept('warning', 'Example warning, just ignore', { node: model, property: 'name' });
         }
     }
 

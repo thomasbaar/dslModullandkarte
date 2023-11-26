6387fbedec38edc82d7c9baebb7e452e96471988
Thomas Pompe <s0525169@htw-berlin.de>
2023-11-26 13:30:39 +0100

    First steps of visualisation, show ev works, detailed view with show NV is not implemented, basically 2 of 4 wireframes implemented

---
+++ mlk/vue-frontend/src/components/CurriculumVisualisation.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <svg style="display: none">
+      <defs>
+        <marker
+          id="arrow-black"
+          markerWidth="10"
+          markerHeight="10"
+          refX="10"
+          refY="3"
+          orient="auto"
+          markerUnits="strokeWidth"
+        >
+          <path d="M0,0 L0,6 L9,3 z" fill="rgb(118, 185, 0)" />
+        </marker>
+      </defs>
+    </svg>
+
+    <div class="vue-flow-container">
+      <VueFlow v-if="isLoaded" :nodes="nodes" :edges="edges" fit-view-on-init>
+        <MiniMap position="bottom-left" />
+        <Controls position="top-right" />
+        <Panel position="bottom-right">
+          <label class="toggle-label">
+            <input type="checkbox" class="toggle-checkbox" />
+            Show NV
+          </label>
+          <label class="toggle-label">
+            <input
+              type="checkbox"
+              class="toggle-checkbox"
+              @change="toggleEdges"
+            />
+            Show EV
+          </label>
+        </Panel>
+      </VueFlow>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueFlow, Position, MarkerType, Panel } from "@vue-flow/core";
+import { Controls } from "@vue-flow/controls";
+import '@vue-flow/controls/dist/style.css'
+import { MiniMap } from "@vue-flow/minimap";
+import { ref, onMounted } from "vue";
+import { useAstStore } from "src/stores/astStore";
+import "@vue-flow/core/dist/style.css";
+import "@vue-flow/core/dist/theme-default.css";
+
+export default {
+  name: "CurriculumVisualisation",
+  components: { VueFlow, MiniMap, Panel, Controls },
+  setup() {
+    const astStore = useAstStore();
+    const isLoaded = ref(false);
+    const nodes = ref([]);
+    const edges = ref([]);
+    const showEdges = ref(false);
+
+
+    const toggleEdges = () => {
+      showEdges.value = !showEdges.value;
+      updateEdges();
+    };
+
+    const updateEdges = () => {
+      const data = astStore.curriculumData;
+      if (data && showEdges.value) {
+        edges.value = data.suppInfo.map((supp) => ({
+          id: `e${supp.predec}-${supp.succ}`,
+          source: supp.predec,
+          target: supp.succ,
+          markerEnd: { type: MarkerType.Arrow, color: "green" },
+          style: { strokeWidth: "3px", stroke: "green" },
+        }));
+      } else {
+        edges.value = [];
+      }
+    };
+
+    onMounted(() => {
+      const data = astStore.curriculumData;
+      if (data) {
+        const tempNodes = [];
+        for (let semester = 1; semester <= data.noSemesters; semester++) {
+          tempNodes.push({
+            id: `semester-${semester}`,
+            label: `Semester ${semester}`,
+            position: { x: (semester - 1) * 250, y: 0 },
+            style: {
+              width: "200px",
+              height: `${
+                data.modules.filter((m) => m.semester === semester).length *50 +60 }px`,
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              border: "2px solid",
+            },
+            connectable: false,
+          });
+
+          data.modules
+            .filter((m) => m.semester === semester)
+            .forEach((mod, index) => {
+              tempNodes.push({
+                id: mod.name,
+                label: mod.shortName,
+                position: { x: 25, y: index * 50 + 50 },
+                extent: "parent",
+                parentNode: `semester-${semester}`,
+                sourcePosition: Position.Right,
+                targetPosition: Position.Left,
+              });
+            });
+        }
+
+        nodes.value = tempNodes;
+        updateEdges();
+      }
+
+      setTimeout(() => {
+        isLoaded.value = true;
+      }, 50);
+    });
+
+    return { nodes, edges, isLoaded, toggleEdges };
+  },
+};
+</script>
+
+<style scoped>
+.vue-flow-container {
+  background-color: #f8f8f8;
+  width: 1620px;
+  height: 875px;
+}
+
+.toggle-label {
+  display: flex;
+  align-items: center;
+  background-color: green;
+  color: white;
+  padding: 15px 30px;
+  border: none;
+  border-radius: 5px;
+  font-size: 24px;
+  cursor: pointer;
+  margin-bottom: 5px;
+}
+
+.toggle-checkbox {
+  margin-right: 15px;
+  transform: scale(1.5);
+}
+
+</style>
--- mlk/vue-frontend/src/layouts/MainLayout.vue
+++ mlk/vue-frontend/src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header elevated class="bg-green">
       <q-toolbar>
         <q-btn
           flat
@@ -15,7 +15,7 @@
           Modullandkarte
         </q-toolbar-title>
 
-        <div>HTW Berlin</div>
+        <div style="font-size: 1.5em;">HTW Berlin</div>
       </q-toolbar>
     </q-header>
 
@@ -25,7 +25,11 @@
       bordered
     >
       <q-list>
-        <q-item clickable @click="showLangium = true; showVisualization = false">
+        <q-item-label header class="text-h6 menu-header">
+          Menu
+        </q-item-label>
+
+        <q-item clickable @click="showLangium = true; showVisualization = false; showCurriculum = false">
           <q-item-section avatar>
             <q-icon name="description" />
           </q-item-section>
@@ -33,23 +37,34 @@
             Langium Input
           </q-item-section>
         </q-item>
-        <q-item clickable @click="showLangium = false; showVisualization = true">
+        <q-item clickable @click="showLangium = false; showVisualization = true; showCurriculum = false">
           <q-item-section avatar>
-            <q-icon name="show_chart" />
+            <q-icon name="text_snippet" />
           </q-item-section>
           <q-item-section>
-            Visualisierung
+            Textuelle Visualisierung
+          </q-item-section>
+        </q-item>
+        <q-item clickable @click="showLangium = false; showVisualization = false; showCurriculum = true">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            Curriculum Visualisation
           </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container >
       <div v-if="showLangium">
-        <iframe src="http://localhost:3000" style="width: 100%; height: 600px;" frameborder="0"></iframe>
+        <iframe src="http://localhost:3000" style="width: 1380px; height: 830px;" frameborder="0"></iframe>
       </div>
       <div v-if="showVisualization">
         <AstVisualizer />
+      </div>
+      <div v-if="showCurriculum" >
+        <CurriculumVisualisation />
       </div>
     </q-page-container>
   </q-layout>
@@ -58,29 +73,39 @@
 <script>
 import { defineComponent, ref } from 'vue'
 import AstVisualizer from 'src/components/AstVisualizer.vue'
+import CurriculumVisualisation from 'src/components/CurriculumVisualisation.vue'
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    AstVisualizer
+    AstVisualizer,
+    CurriculumVisualisation
   },
 
   setup() {
-    const leftDrawerOpen = ref(false)
-    const showLangium = ref(true)
-    const showVisualization = ref(false)
+    const leftDrawerOpen = ref(false);
+    const showLangium = ref(true);
+    const showVisualization = ref(false);
+    const showCurriculum = ref(false);
 
     function toggleLeftDrawer() {
-      leftDrawerOpen.value = !leftDrawerOpen.value
+      leftDrawerOpen.value = !leftDrawerOpen.value;
     }
 
     return {
       leftDrawerOpen,
       showLangium,
       showVisualization,
+      showCurriculum,
       toggleLeftDrawer
-    }
+    };
   }
-})
+});
 </script>
+
+<style scoped>
+.menu-header {
+  color: black;
+}
+</style>
